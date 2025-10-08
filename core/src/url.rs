13e32efe8b1b060a3aa9bfb7d723fef85cd25c02
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2025-10-08 12:57:18 +0900

    URLのクエリパラメータまで取得可能、testコード作成

--- core/src/url.rs
+++ core/src/url.rs
@@ -1,4 +1,7 @@
 use alloc::string::{String, ToString};
+use alloc::vec::Vec;
+
+// http://<host>:<port>/<path>?<searchpart>
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Url {
@@ -29,6 +32,13 @@ impl Url {
         if !self.is_http() {
             return Err("Only HTTP scheme is supported.".to_string());
         }
+
+        self.host = self.extract_host();
+        self.port = self.extract_port();
+        self.path = self.extract_path();
+        self.searchpart = self.extract_searchpart();
+
+        Ok(self.clone())
     }
 
     // httpスキーマを省略していないかの判定
@@ -38,7 +48,7 @@ impl Url {
 
     // host部分を抽出するメソッド
     fn extract_host(&self) -> String {
-        let url_patrs: Vec<&str> = self
+        let url_parts: Vec<&str> = self
             .url
             .trim_start_matches("http://")
             .splitn(2, "/")
@@ -67,5 +77,169 @@ impl Url {
             // httpのデフォルトポートは80番
             "80".to_string()
         }
+    }
+
+    // path部分を抽出するメソッド
+    fn extract_path(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        // pathが存在しない場合は空の文字列を返す
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        // 左側がpath、右側がsearchpart
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        path_and_searchpart[0].to_string()
+    }
+
+    // searchpart部分を抽出するメソッド
+    fn extract_searchpart(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        // pathが存在しない場合は空の文字列を返す
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        // 左側がpath、右側がsearchpart
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+
+        // queryパラメータが存在しない場合はからの文字列を返す
+        if path_and_searchpart.len() < 2 {
+            "".to_string()
+        } else {
+            path_and_searchpart[1].to_string()
+        }
+    }
+
+    // rustの構造体はデフォルトでプライベートなので、外部からアクセスできるようにゲッターを定義する
+    // 以降はゲッターメソッド
+
+    pub fn host(&self) -> String {
+        self.host.clone()
+    }
+
+    pub fn port(&self) -> String {
+        self.port.clone()
+    }
+
+    pub fn path(&self) -> String {
+        self.path.clone()
+    }
+
+    pub fn searchpart(&self) -> String {
+        self.searchpart.clone()
+    }
+}
+
+// ユニットテスト（実装したコードの一部が正しい挙動をしているかの確認）による動作確認
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // 成功ケース
+
+    // host名が記入されている
+    #[test]
+    fn test_url_host() {
+        let url = "http://example.com".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "80".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    // host名とport番号が記入されている
+    #[test]
+    fn test_url_host_port() {
+        let url = "http://exapmle.com:8888".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "exapmle.com".to_string(),
+            port: "8888".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    // host名とport番号、pathが記入されている
+    #[test]
+    fn test_url_host_port_path() {
+        let url = "http://example.com:8888/index.html".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    // host名とpathが記入されている
+    #[test]
+    fn test_url_host_path() {
+        let url = "http://example.com/index.html".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "80".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    // host名、port番号、path、searchpartが記入されている
+    #[test]
+    fn test_url_host_port_path_searchpart() {
+        let url = "http://example.com:8888/index.html?a=123&b=456".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "a=123&b=456".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    // 失敗ケース
+
+    // scheme(HTTP)が記入されていない
+    #[test]
+    fn test_no_scheme() {
+        let url = "example.com".to_string();
+        let expected = Err("Only HTTP scheme is supported.".to_string());
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    // HTTP以外のschemeを記入している
+    #[test]
+    fn test_unsupported_scheme() {
+        let url = "https://example.com:8888/index.html?a=123&b=456".to_string();
+        let expected = Err("Only HTTP scheme is supported.".to_string());
+
+        assert_eq!(expected, Url::new(url).parse());
     }
 }
